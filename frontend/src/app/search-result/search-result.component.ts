c7ce7de8bcc5372e61b3df264d3d2209d68f3998
Alisa Arkalykova <49148189+alisiska@users.noreply.github.com>
2024-10-09 23:36:55 +0200

    update

--- frontend/src/app/search-result/search-result.component.ts
+++ frontend/src/app/search-result/search-result.component.ts
@@ -1,4 +1,19 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repository: https://github.com/juice-shop/juice-shop
+*    Source File: frontend/src/app/search-result/search-result.component.ts
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
